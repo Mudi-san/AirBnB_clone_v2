253dc62654a07797838508e57f4309e13780f498
Gsoom009 <mohamedgasim9797@gmail.com>
2023-12-21 02:13:27 +0300

    ghmwddssdxzsdeexsd

--- models/user.py
+++ models/user.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-"""This is the state class"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
 
 
-class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all,delete", backref="state")
-
-    @property
-    def cities(self):
-        """  getter attribute cities that returns the list of
-        City instances with state_id equals to the current State.id """
-        from models import storage
-        allcities = storage.all(City)
-        c = []
-        for k, v in allcities.items():
-            if v.state_id == self.id:
-                c.append(v)
-        return c
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete, delete-orphan')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
