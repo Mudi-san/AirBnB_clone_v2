6f1288b09dc355a542f887757ae63a09234dc559
Mudi-san <mudijewilly@gmail.com>
2024-01-11 19:18:07 +0200

    Mudi is Done

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,35 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-# Fabfile to delete out-of-date archives.
-import os
-from fabric.api import *
-
-env.hosts = ["104.196.168.90", "35.196.46.172"]
-
-
-def do_clean(number=0):
-    """Delete out-of-date archives.
-
-    Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
-=======
 """
 do_pack(): Generates a .tgz archive from the
 contents of the web_static folder
@@ -126,4 +95,3 @@ def do_clean(number=0):
             continue
         run("rm -rf /data/web_static/releases/{}"
             .format(i))
->>>>>>> f262a14c8808f873373d4e853121952b0b87b1a4
